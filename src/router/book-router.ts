de095acf55a4baebdc99860ed180a6d222c1a2f4
lalatina11 <rahmadancandra@gmail.com>
2025-09-01 21:55:42 +0700

    feat: add book router for get all and create book

--- src/router/book-router.ts
+++ src/router/book-router.ts
@@ -1,13 +1,11 @@
 import { Router } from "express";
 
 import BookController from "../controller/book-controller.js";
+import { authMiddleware } from "../middlewares.js";
 
 const router = Router();
 
-router.get("/", () => {
-
-});
-
-router.post("/", BookController.create);
+router.get("/", BookController.getAllBooks);
+router.post("/", authMiddleware, BookController.create);
 
 export default router;
