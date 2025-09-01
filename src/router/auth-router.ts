a9c5c4617f79b200d268aa15268e436090fb423a
lalatina11 <rahmadancandra@gmail.com>
2025-09-01 21:56:23 +0700

    refactor: using auth middleware for getting current is logged in user

--- src/router/auth-router.ts
+++ src/router/auth-router.ts
@@ -1,10 +1,11 @@
 import express from "express";
 
 import AuthController from "../controller/auth-controller.js";
+import { authMiddleware } from "../middlewares.js";
 
 const route = express.Router();
 
-route.get("/current-user", AuthController.getCurrentUser);
+route.get("/current-user", authMiddleware, AuthController.getCurrentUser);
 
 route.post("/sign-up", AuthController.signUp);
 
