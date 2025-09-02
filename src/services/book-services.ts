ca142230164a7f52e3095de69ca7547cb7bf25a4
lalatina11 <rahmadancandra@gmail.com>
2025-09-02 08:21:37 +0700

    feat: add book image uploader services

--- src/services/book-services.ts
+++ src/services/book-services.ts
@@ -1,5 +1,10 @@
+import type { UploadedFile } from "express-fileupload";
+
+import streamifier from "streamifier";
+
 import type { BookFields } from "../types/fields.js";
 
+import cloudinary from "../lib/cloudinary.js";
 import { BookRepository } from "../repositories/book-repositories.js";
 
 export const BookServices = {
@@ -14,5 +19,21 @@ export const BookServices = {
     }
     const { _id: id } = newBook;
     return BookRepository.findById(id);
+  },
+  bookImageUploader: async (image: UploadedFile | UploadedFile[]) => {
+    const uploadResult = await new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        { folder: "books" },
+        (error, result) => {
+          if (error)
+            reject(error);
+          else resolve(result);
+        },
+      );
+
+      streamifier.createReadStream((image as any).data).pipe(uploadStream);
+    });
+
+    return (uploadResult as any).secure_url;
   },
 };
