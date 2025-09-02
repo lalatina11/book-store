6f3b9ba0295ba2b801e52c8f1919273d0f3b880f
lalatina11 <rahmadancandra@gmail.com>
2025-09-02 08:22:02 +0700

    feat: add book update and delete router

--- src/router/book-router.ts
+++ src/router/book-router.ts
@@ -7,5 +7,7 @@ const router = Router();
 
 router.get("/", BookController.getAllBooks);
 router.post("/", authMiddleware, BookController.create);
+router.patch("/:id", authMiddleware, BookController.update);
+router.delete("/:id", authMiddleware, BookController.delete);
 
 export default router;
